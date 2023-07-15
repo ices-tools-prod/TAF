4bbb7e231ff4c0884bd358cdcfb78f76c8a51a71
Iago Mosqueira <iagomosqueira@users.noreply.github.com>
2023-07-15 16:37:34 +0200

    Suggested taf.libraries()

---
+++ R/taf.libraries.R
@@ -0,0 +1,44 @@
+#' TAF Libraries
+#'
+#' Load and attach all packages from local TAF library.
+#'
+#' @param messages whether to show messages when package loads.
+#' @param warnings whether to show warnings when package loads.
+#'
+#' @return TRUE (invisibly) if all packages loaded
+#'
+#' @note
+#' Packages in the TAF library are loaded in the order in which they are
+#' listed in SOFTWARE.bib. Internal dependencies can in this way be respected.
+#'
+#' @seealso
+#' \code{\link{taf.library}} is the TAF function called for each found package.
+#'
+#' @examples
+#' \dontrun{
+#'
+#' # Load all packages in TAF library
+#' taf.libraries()
+#'
+#' }
+#'
+#' @export
+
+taf.libraries <- function(messages=FALSE, warnings=FALSE) {
+
+  bib <- TAF:::read.bib(file.path(boot.dir(), "SOFTWARE.bib"))
+  
+  entries <- names(bib)
+
+  installed <- taf.library()
+
+  pkgs <- entries[entries %in% installed]
+
+  res <- lapply(pkgs, function(x) {
+    if(dir.exists(file.path(boot.dir(), "library", x)))
+      do.call("taf.library", list(package=x, messages=messages,
+        warnings=warnings))
+    })
+
+  invisible(TRUE)
+}
