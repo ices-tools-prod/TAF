44aeb160bea995fb303e954336529d567dd1b8cc
Arni Magnusson <thisisarni@gmail.com>
2023-10-12 16:19:50 +1100

    Resolve R CMD check NOTE about colons

--- R/taf.libraries.R
+++ R/taf.libraries.R
@@ -5,33 +5,31 @@
 #' @param messages whether to show messages when package loads.
 #' @param warnings whether to show warnings when package loads.
 #'
-#' @return TRUE (invisibly) if all packages loaded
+#' @return \code{TRUE} (invisibly) if all packages loaded.
 #'
 #' @note
 #' Packages in the TAF library are loaded in the order in which they are
-#' listed in SOFTWARE.bib. Internal dependencies can in this way be respected.
+#' listed in \verb{SOFTWARE.bib}. Internal dependencies can in this way be
+#' respected.
 #'
 #' @seealso
 #' \code{\link{taf.library}} is the TAF function called for each found package.
 #'
+#' \code{\link{TAF-package}} gives an overview of the package.
+#'
 #' @examples
 #' \dontrun{
-#'
 #' # Load all packages in TAF library
 #' taf.libraries()
-#'
 #' }
 #'
 #' @export
 
 taf.libraries <- function(messages=FALSE, warnings=FALSE) {
 
-  bib <- TAF:::read.bib(file.path(boot.dir(), "SOFTWARE.bib"))
-  
+  bib <- read.bib(file.path(boot.dir(), "SOFTWARE.bib"))
   entries <- names(bib)
-
   installed <- taf.library()
-
   pkgs <- entries[entries %in% installed]
 
   res <- lapply(pkgs, function(x) {
