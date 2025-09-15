ba1032e352b8850013697c3cdbc1f8cccd710842
Arni Magnusson <thisisarni@gmail.com>
2025-09-15 15:47:26 +1100

    Add comments in wide2long()

--- R/wide2long.R
+++ R/wide2long.R
@@ -33,15 +33,25 @@
 
 wide2long <- function(x, names=c("Age", "Value"))
 {
+  # Select left columns and save their values as row labels
   cols <- grep("[0-9]", names(x), value=TRUE, invert=TRUE)
   rowlab <- apply(x[cols], 1, paste, collapse = "|")
   x <- x[!names(x) %in% cols]
   row.names(x) <- rowlab
+
+  # Convert crosstab to long format
   x <- as.data.frame(as.table(as.matrix(x)), stringsAsFactors=FALSE)
+
+  # Simplify data type
   x[[2]] <- type.convert(as.character(x[[2]]), as.is=TRUE)
+
+  # Restore left columns
   left <- read.table(text=x[[1]], sep = "|", col.names=cols)
   x[[1]] <- NULL
+
+  # Apply names and combine
   names(x) <- names
   x <- cbind(left, x)
+
   x
 }
